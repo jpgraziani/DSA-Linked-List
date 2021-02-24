2fc3c3f87e50ad60b67723fe96e0618547996420
Jonathan Graziani <jpgraziani@gmail.com>
2021-02-24 10:49:37 -0700

    LinkedList added

--- linkedList.js
+++ linkedList.js
@@ -29,4 +29,57 @@ class LinkedList {
       tempNode.next = new _Node(item, null)
     }
   }
+
+  find(item) {
+    //stat at the head
+    let currNode = this.head;
+    //if the list is empty
+    if (!this.head) {
+      return null;
+    }
+
+    //check for the item
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        currNode = currNode.next;
+      }
+    }
+
+    //found it
+    return currNode;
+  }
+
+  remove(item) {
+    if (!this.head) {
+      return null;
+    }
+
+    //if node being removed at head make next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+
+    //start at the head
+    let currNode = this.head;
+    //keep track of previous
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)) {
+      //save the previous node
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode == null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
 }
+
+//best case is O(1)
+//average or worst are O(n) due to finding the node that you want to remove
